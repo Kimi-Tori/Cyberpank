1d073cc0c73863fceaf25b9f088f56790ba0276c
KimiTori <nn_322@mail.ru>
2024-08-21 18:01:35 +0300

    test gh-pages url home page

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  publicPath: `/home/`,
+  publicPath: `/Cyberpank/`,
   transpileDependencies: true,
   css: {
     loaderOptions: {
