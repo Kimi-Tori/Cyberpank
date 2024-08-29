5bd7046557d27f88a3c674c2b047aaefe6688c16
KimiTori <nn_322@mail.ru>
2024-08-29 14:15:16 +0300

    last commit

--- src/components/elements/form-template.vue
+++ src/components/elements/form-template.vue
@@ -1,78 +1,65 @@
 <template>
-    <form class="form" @submit.prevent="v$.value.$validate">
-        <v-text-field
-            class="form__input Roboto-18-300 color-white"
-            v-model="state.name"
-            :counter="10"
-            :error-messages="v$.value?.name?.$errors.map(e => e.$message) || []"
-            label="Как тебя зовут?"
-            required
-            @blur="v$.value?.name?.$touch"
-            @input="v$.value?.name?.$touch"
-        ></v-text-field>
+  <v-form class="form" v-model="valid">
+    <v-text-field
+      class="form__input Roboto-18-300 color-white"
+      v-model="firstname"
+      :rules="nameRules"
+      label="Как тебя зовут?"
+      required
+    ></v-text-field>
 
-        <v-text-field
-            class="form__input Roboto-18-300 color-white mt-3"
-            v-model="state.email"
-            :error-messages="v$.value?.email?.$errors.map(e => e.$message) || []"
-            label="Твой е-mail"
-            required
-            @blur="v$.value?.email?.$touch"
-            @input="v$.value?.email?.$touch"
-        ></v-text-field>
+    <v-text-field
+      class="form__input Roboto-18-300 color-white mt-3"
+      v-model="email"
+      :rules="emailRules"
+      label="Твой е-mail"
+      required
+    ></v-text-field>
 
-        <v-checkbox
-            class="form__input Roboto-18-300 color-white mt-3"
-            v-model="state.checkbox"
-            :error-messages="v$.value?.checkbox?.$errors.map(e => e.$message) || []"
-            label="Согласен на обработку персональных данных"
-            required
-            @change="v$.value?.checkbox?.$touch"
-        ></v-checkbox>
-  
-      <v-btn class="form__btn Roboto-18-700 text-capitalize color-black py-5 px-10 mt-3" type="submit">Отправить</v-btn>
-    </form>
+    <v-checkbox
+      class="form__input Roboto-18-300 color-white mt-3"
+      v-model="checkbox"
+      :rules="checkboxRules"
+      label="Согласен на обработку персональных данных"
+      required
+    ></v-checkbox>
+
+    <v-btn class="form__btn Roboto-18-700 text-capitalize color-black py-5 px-10 mt-3" type="submit">Отправить</v-btn>
+  </v-form>
 </template>
 
 <script>
-import { reactive, watchEffect } from 'vue'
-import { useVuelidate } from '@vuelidate/core'
-import { email, required } from '@vuelidate/validators'
-
 export default {
   name: 'form-template',
+  data: () => ({
+    checkbox: false,
+    valid: false,
+    firstname: '',
+    lastname: '',
+    nameRules: [
+      value => {
+        if (value) return true
 
-  setup() {
-    const initialState = {
-      name: '',
-      email: '',
-      checkbox: null,
-    }
+        return 'Введите ваше имя'
+      },
+    ],
+    email: '',
+    emailRules: [
+      value => {
+        if (value) return true
 
-    const state = reactive({ ...initialState })
+        return 'Введите ваш e-mail'
+      },
+      value => {
+        if (/.+@.+\..+/.test(value)) return true
 
-    const rules = {
-      name: { required },
-      email: { required, email },
-      checkbox: { required },
-    }
-
-    const v$ = useVuelidate(rules, state)
-
-    function clear() {
-      v$.value.$reset()
-
-      for (const [key, value] of Object.entries(initialState)) {
-        state[key] = value
-      }
-    }
-
-    return {
-      state,
-      v$,
-      clear,
-    }
-  }
+        return 'Ваш e-mail выглидит не корректно'
+      },
+    ],
+    checkboxRules: [
+      value => !!value || 'Согласитесь на обработку персональных данных',
+    ],
+  }),
 }
 </script>
 
